e3e6505d450eb0d44e0501d8ee630348ae5cde47
JakeDahl <jakejdahl@gmail.com>
2019-08-03 22:58:33 -0400

    Update common.h

--- MC.dll/common.h
+++ MC.dll/common.h
@@ -6,13 +6,11 @@
 #include <Rpc.h>
 #include <iphlpapi.h>
 
+//Not all are used... old imports.
 #pragma comment(lib, "Rpcrt4.lib")
 #pragma comment(lib, "MinHook.lib")
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "IPHLPAPI.lib")
-
-//DWORD findPattern(HANDLE processHandle, const unsigned char pattern[], const char* mask, const int offset, size_t begin);
-//DWORD FindPattern(char* module, char* pattern, char* mask);
 
 void WINAPI ConsoleInit();
 void InstallHooks();
